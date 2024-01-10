e3ad6ca4c49f7649ab611725bb08d08e9d5cf5b5
Belikov <smartcompany11@gmail.com>
2024-01-10 20:09:40 +0600

    Added read and write file json

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import entities.Task;
+import utils.FileUtil;
+
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Task> tasks = new ArrayList<>();
+//        for (int i = 0; i < 5; i++) {
+//            tasks.add(new Task(("Task " + i), "Test", LocalDate.now().plusDays(new Random().nextLong(15)), Priority.MEDIUM  ));
+//
+//        }
+
+        try {
+            tasks.addAll(FileUtil.readFromFile());
+
+        } catch (NoSuchFileException noSuchFileException) {
+            System.out.println("Файл не создан.");
+        } catch (IOException ioException) {
+            System.err.println(ioException.getMessage());
+        }
+
+
+        tasks.forEach(
+                System.out::println
+        );
+
+        System.out.println(tasks);
+
+//        FileUtil.writeToFile(tasks);
     }
 }
---
+++ src/utils/FileUtil.java
@@ -0,0 +1,48 @@
+package utils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import entities.Task;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FileUtil {
+
+    private static final Gson GSON = new GsonBuilder()
+            .setDateFormat("dd.MM.yyyy")
+//            .setPrettyPrinting().registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter())
+            .create();
+    private static final Path PATH = Path.of("data/tasks.json");
+
+    private FileUtil() {
+    }
+
+
+    public static List<Task> readFromFile() throws IOException {
+        Type taskType = new TypeToken<Map<String, List<Task>>>() {
+        }.getType();
+        Map<String, List<Task>> map = GSON.fromJson(Files.readString(PATH), taskType);
+        return map.get("tasks");
+    }
+
+    public static void writeToFile(List<Task> tasks) {
+        Map<String, List<Task>> map = new HashMap<>();
+        map.put("tasks", tasks);
+
+        try (FileWriter writer = new FileWriter(String.valueOf(PATH))) {
+            GSON.toJson(map, writer);
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+}

60c4580711e825e483e595b99acab4054bed5c40
Belikov <smartcompany11@gmail.com>
2024-01-10 20:41:55 +0600

    implement Builder

--- src/entities/Task.java
+++ src/entities/Task.java
@@ -3,26 +3,41 @@ package entities;
 import enums.Priority;
 import enums.Status;
 
+import java.sql.Date;
 import java.time.LocalDate;
 
 public class Task {
     private String title;
     private String description;
-    private LocalDate completionDate;
-    private LocalDate createdDate;
+    private Date completionDate;
+    private Priority priority;
+    private Date createdDate;
     private transient Status statusObj;
     private String status;
-    private Priority priority;
+
+    protected Task() {
+        this.statusObj = Status.NEW;
+        this.status = statusObj.toString();
+        this.createdDate = Date.valueOf(LocalDate.now());
+    }
 
     public Task(String title, String description, LocalDate completionDate, Priority priority) {
         this.title = title;
         this.description = description;
-        this.completionDate = completionDate;
+        this.completionDate = Date.valueOf(completionDate);
         this.priority = priority;
 
         this.statusObj = Status.NEW;
         this.status = statusObj.toString();
-        this.createdDate = LocalDate.now();
+        this.createdDate = Date.valueOf(LocalDate.now());
+    }
+
+    public void setStatusObj(Status statusObj) {
+        this.statusObj = statusObj;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     @Override
@@ -36,5 +51,38 @@ public class Task {
                 + ",         \"status\":\"" + status + "\""
                 + ",         \"priority\":\"" + priority + "\""
                 + "}}";
+    }
+
+
+    public static class TaskBuilder {
+        private final Task newTask;
+
+        public TaskBuilder() {
+            this.newTask = new Task();
+        }
+
+        public TaskBuilder title(String title) {
+            newTask.title = title;
+            return this;
+        }
+
+        public TaskBuilder description(String desc) {
+            this.newTask.description = desc;
+            return this;
+        }
+
+        public TaskBuilder completionDate(LocalDate date) {
+            newTask.completionDate = Date.valueOf(date);
+            return this;
+        }
+
+        public TaskBuilder priority(Priority priority) {
+            newTask.priority = priority;
+            return this;
+        }
+
+        public Task build() {
+            return newTask;
+        }
     }
 }
